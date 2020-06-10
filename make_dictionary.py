5e9a8b7dcaddf75930ae85d11ef059223ac084e2
Jozdien <joseverres42@gmail.com>
2020-06-11 01:03:05 +0530

    Accounting for punctuations.

--- make_dictionary.py
+++ make_dictionary.py
@@ -1,27 +1,21 @@
 import json
 from collections import OrderedDict
 
-'''
-f = open('words_dictionary.json')
-data = json.load(f)
-index = "0" # 19-digit binary code, large enough for everything
-dictionary_ntw = []  #Store numbers, special characters too
-dictionary_wtn = []
-'''
-
 f = open("words_unformatted.txt", "r")
-dict_ntw = {}
+dict_ntw = {"0": ".", "1": ",", "10": "'", "11": "\"", "110110": "0", "110111": "1", "111000": "2", "111001": "3", "111010": "4", "111011": "5", "111100": "6", "111101": "7", "111110": "8", "111111": "9", "1000000": "(", "1000001": ")", "1000010": "+", "1000011": "-", "1000100": "*", "1000101": ";", "1000110": ":", "1000111": "!", "1001000": "$", "1001001": "%", "1001010": "&", "1001011": "/"}
 dict_wtn = {}
+#TODO: Fill out the remaining bits with that many digits in the JSON with numbers
+
 for i in f:
 	temp = i.split()[:2]
-	dict_ntw[temp[0]] = temp[1]
-	dict_wtn[temp[1]] = temp[0]
+	dict_ntw[str(bin(int(temp[0]))[2:])] = temp[1]
+	dict_wtn[temp[1]] = str(bin(int(temp[0]))[2:])
 f.close()
 
 json_ntw = json.dumps(OrderedDict(sorted(dict_ntw.items(), key=lambda kv: int(kv[0]))), indent = 4)
 json_wtn = json.dumps(OrderedDict(sorted(dict_wtn.items(), key=lambda kv: int(kv[1]))), indent = 4)
 
 with open("dictionary_wtn.json", "w+") as outfile: 
-    outfile.write(json_ntw)
+    outfile.write(json_wtn)
 with open("dictionary_ntw.json", "w+") as outfile:
-	outfile.write(json_wtn)
+	outfile.write(json_ntw)
--- program.py
+++ program.py
@@ -1,5 +1,15 @@
-words = re.split(r'(\W+)', original) # Split, separate special characters
+import re
+import json
+
+original = "you I to"
+words = re.findall(r"[\w]+|[^\s\w]", original) # Split, separate special characters
 final = ""
+
+with open('dictionary_wtn.json', 'r') as openfile: 
+    dictionary_wtn = json.load(openfile)
+
 for i in words:
-	final = final + dictionary_wtn[i] + " "
+	final = final + dictionary_wtn[i] + "\n"
+final = final[:-1]
+
 print(final)
